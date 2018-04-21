0001794870a30e9c11402b9266a4108e14a5381b
Oksana Tkachenko <oksana@crystax.net>
2018-04-21 09:12:48 +0300

    changed jQuery, when hamburger clicks - menu opens

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('.hamburger').on('click', function(e) {
     e.preventDefault();
     $(this).toggleClass('active');
-    $('#navigation').toggleClass('menu-mobile');
+    $('#navigation').toggleClass('nav-menu--menu-mobile');
   });
 });
